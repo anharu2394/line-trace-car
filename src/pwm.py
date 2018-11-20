283197b8f0f18a50fa8b34ca8e9c0965ce682279
Anharu <haruan2394@gmail.com>
2018-11-20 17:40:47 +0900

    Add: pwm and main

---
+++ src/pwm.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+import Adafruit_PCA9685
+
+# 0 1 2 sorvp
+# 3 ~ A dc motor
+# 3 right-front front
+
+# 6 left-front back
+# 7 right-back back
+# 8 right-back front
+# 9 left-back front
+# 10(A) left-back back
+pwm = Adafruit_PCA9685.PCA9685()
+pwm.set_pwm_freq(60)
+
+while True:
+    #val = int(input("> "))
+    #pwm.set_pwm(0, 0, 600)
+   # time.sleep(1)
+    pwm.set_pwm(5, 0, 1000)
+    time.sleep(1)
+    #pwm.set_pwm(3, 0, 1000)
+    #time.sleep(1)
+    #pwm.set_pwm(1, 0, 100)
+
+
+
